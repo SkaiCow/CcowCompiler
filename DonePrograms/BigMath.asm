d36627c3e2afad231ad239fde61aa28ad021a9e6
SkaiCow <Willalasater@gmail.com>
2023-05-01 11:52:19 -0500

    nested If Test

--- DonePrograms/BigMath.asm
+++ DonePrograms/BigMath.asm
@@ -1,8 +1,15 @@
 section .data
-number dd 0
-results dd 0
-Lit0 dd 0
-Lit1 dd 1
+a dd 0
+Bob dd 0
+Jane dd 0
+b dd 0
+c dd 0
+Lit20 dd 20
+Lit26 dd 26
+ans dd 0
+Lit10 dd 10
+Lit2 dd 2
+Lit4 dd 4
 T1 dd 0
 T2 dd 0
 T3 dd 0
@@ -36,28 +43,48 @@ section .bss
     negflag RESB 1
 global _start
 section .text
-_start:
-main: nop
+_start: nop
+mov ax,[Lit20]
+mov [Bob],ax
+mov ax,[Lit26]
+mov [Jane],ax
 call PrintString
 call GetAnInteger
-mov [number],eax
-mov ax,[Lit0]
-mov [results],ax
-mov ax,[Lit2]
-cmp ax,[number]
-JLE L1
-mov ax,[Lit3]
-cmp ax,[number]
-JLE L2
+mov [a],eax
+call PrintString
+call GetAnInteger
+mov [b],eax
+call PrintString
+call GetAnInteger
+mov [c],eax
+mov ax,[Jane]
+add ax,[Bob]
+mov [T1],ax
+mov ax,[T1]
+sub ax,[Lit10]
+mov [T2], ax
+mov dx, 0
+mov ax, [T2]
+mov bx, [Lit2]
+div bx
+mov [T1], ax
 mov ax,[Lit4]
-cmp ax,[number]
-JL L3
-mov ax,[Lit1]
-mov [results],ax
-L3: nop
-L2: nop
-L1: nop
-mov ax, [results]
+mul byte[T1]
+mov [T2], ax
+mov ax,[T2]
+mul byte[a]
+mov [T1], ax
+mov ax,[c]
+add ax,[b]
+mov [T3],ax
+mov dx, 0
+mov ax, [T1]
+mov bx, [T3]
+div bx
+mov [T4], ax
+mov ax,[T4]
+mov [ans], ax
+mov ax, [ans]
 call ConvertIntegerToString
 mov eax, 4
 mov ebx, 1
